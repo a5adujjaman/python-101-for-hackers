751271e630aac6831cfeaad4c7222d0fed9b8346
a5adujjaman <asadujjamanasif46567@gmail.com>
2025-03-16 00:11:20 +0600

    Initial commit

---
+++ calcualte-demo.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+print(1+1)
+print(__name__)
+if __name__ == "__main__":
+    print("do something")
---
+++ syntax-demo.py
@@ -0,0 +1,14 @@
+def test():
+ print("test")
+ print("test2")
+
+ string = "string"
+ number = 10
+
+test()
+
+x = 1+2+3
+print(x)
+y = 1  +    1
+
+print(y)
